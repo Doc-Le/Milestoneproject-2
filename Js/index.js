846fb930378da03f0775a36a5e593874677f6df7
Leandro De Araujo <leandro.arj@outlook.com>
2021-06-20 21:16:14 +0100

    Add function to show splash screen

--- Js/index.js
+++ Js/index.js
@@ -4,30 +4,36 @@
  *  @author Leandro De Araujo
  */
 
-/** 
- * Global variables 
- */
+/** Global variables */
+const $board = $('#board');
+const $main = $('#main');
+const $splash = $('#splash');
+const $playButton = $('#play');
+const $quitButton = $('#quit');
+const $restartButton = $('#restart');
+const $saveButton = $('#save');
+const $gamePanel = $('#gamePanel');
+const $menuPanel = $('#menuPanel');
+const allCards = [];
+/** 2s timeout transition for splash screen */
+const splashScreenTimeout = 2000;
+let cards = [];
 
- const $board = $('#board');
- const $container = $('#container');
- const $splash = $('#splash');
- const $playButton = $('#play');
- const $quitButton = $('#quit');
- const $restartButton = $('#restart');
- const $saveButton = $('#save');
- const $gamePanel = $('#gamePanel');
- const $menuPanel = $('#menuPanel');
- const allCards = [];
- 
- let cards = [];
- 
- 
- /** Function to initialize game context */
- function init() {
-     // Initialize context DOM elements with jQuery
-     // Reset game panels
-     // Attach events to context DOM elements
- }
- 
- /** JQuery detects state of readiness and call initilize */
- $(document).ready(init);
+
+/** Function to initialize game context */
+function init() {
+    showSplashScreen();
+    // Attach events to context DOM elements
+}
+
+/** Function to show splash and transition to main screen */
+function showSplashScreen () {
+    $splash.show();
+    $main.hide();
+    const hideSplashScreenTimeout = setTimeout(function () {
+        clearTimeout(hideSplashScreenTimeout);
+    }, splashScreenTimeout);    
+}
+
+/** JQuery detects state of readiness and call initilize */
+$(document).ready(init);
