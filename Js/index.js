d549f39d052035111e196308b0208688e9a1e105
Leandro De Araujo <leandro.arj@outlook.com>
2021-06-20 20:50:09 +0100

    Add initial global variables

---
+++ Js/index.js
@@ -0,0 +1,22 @@
+/**
+ *  Index Javascript file representing the whole game global context
+ *  @file index.js
+ *  @author Leandro De Araujo
+ */
+
+/** 
+ * Global variables 
+ */
+
+ const $board = $('#board');
+ const $container = $('#container');
+ const $splash = $('#splash');
+ const $playButton = $('#play');
+ const $quitButton = $('#quit');
+ const $restartButton = $('#restart');
+ const $saveButton = $('#save');
+ const $gamePanel = $('#gamePanel');
+ const $menuPanel = $('#menuPanel');
+ const allCards = [];
+ 
+ let cards = [];
